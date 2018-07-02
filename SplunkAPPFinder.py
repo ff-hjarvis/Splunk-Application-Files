07eb1134205506a6348b2d29e42d4f817299ced4
Hunter Jarvis <40272147+ff-hjarvis@users.noreply.github.com>
2018-07-02 10:12:46 -0400

    Delete SplunkAPPFinder.py
    Not needed anymore.

--- SplunkAPPFinder.py
+++
@@ -1,6 +0,0 @@
-import os
-
-def find(FirstFuel_Operations,C:\Program Files\Splunk\etc\apps):
-    for root, dirs, files in os.walk(C:\Program Files\Splunk\etc\apps):
-        if name in files:
-            return os.path.join(C:,FirstFuel_Operations)
